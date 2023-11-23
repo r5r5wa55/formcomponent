bafddde42196dd1ec04889c81a7a1be9f7990514
werapol <r5r5wa55@hotmail.com>
2023-11-23 13:42:13 +0700

    css

--- src/components/formcomponent.js
+++ src/components/formcomponent.js
@@ -1,32 +1,35 @@
 
+import "./formcomponent.css"
+
 
 export default function FormComponent(){
 
     return (
         <div className="container">
-            <from className="from">
-                <div className="from-control">
+            <form className="form">
+                <h2>แบบฟอร์มการลงทะเบียน</h2>
+                <div className="form-control">
                     <label>ชื้อผู้ใช้</label>
                     <input type="text"></input>
                     <small>error massage</small>
                 </div>
-                <div className="from-control">
+                <div className="form-control">
                     <label>อีเมล</label>
                     <input type="text"></input>
                     <small>error massage</small>
                 </div>
-                <div className="from-control">
+                <div className="form-control">
                     <label>รหัสผ่าน</label>
                     <input type="text"></input>
                     <small>error massage</small>
                 </div>
-                <div className="from-control">
+                <div className="form-control">
                     <label>ยืนยันรหัสผ่าน</label>
                     <input type="text"></input>
                     <small>error massage</small>
                 </div>
                 <button type="submit">ยืนยัน</button>
-            </from>
+            </form>
         </div>
     )
 
